48f7523417f92ed906aafd7570895cf5df33f6a9
unknown <jinhong.hu@qudian.so>
2013-07-06 15:51:59 +0800

    add smoothscroll effect

--- js/main.js
+++ js/main.js
@@ -1,3 +1,46 @@
 $(document).ready(function(){
 
+  initLayout();
+
+  $(".step1").click(function(){
+    setSmoothScroll('#makeorder')
+  });
+
+  $(".step2").click(function(){
+    setSmoothScroll('#welcome')
+  });
+
+  $(".step3").click(function(){
+    setSmoothScroll('#success')
+  });
+
+  $(".step4").click(function(){
+    setSmoothScroll('#makeorder')
+  });
+
+  $(".step5").click(function(){
+    
+  });
+
 });
+
+function setSmoothScroll(target) {
+  $.smoothScroll({
+    direction: 'left',
+    scrollTarget: target,
+    beforeScroll: function() {
+
+    },
+    afterScroll: function() {
+      
+    }
+  });
+}
+
+function initLayout() {
+  // console.log(document.body.offsetWidth + '-'+ 
+  //  document.body.offsetHeight);
+
+    var w = document.body.offsetWidth + 'px';
+    $(".pane").css("width", w);
+}
