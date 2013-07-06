bc9b791380d8738af1c03a218f1e288eae285879
unknown <jinhong.hu@qudian.so>
2013-07-06 17:52:09 +0800

    finish history tab demo

--- js/history.js
+++ js/history.js
@@ -1,7 +1,16 @@
 $(document).ready(function(){
   $(".order-item").click(function(){
-    $(".order-detail").toggle(function(){
-      
-    });
+    $(".order-detail").toggle();
   });
+
+  $(".list-title").click(function(){
+    $(".order-list").show();
+    $(".analysis-list").hide();
+  });
+
+  $(".analysis-title").click(function(){
+    $(".order-list").hide();
+    $(".analysis-list").show();
+  });
+
 });
